dca510b7637c57bda9a8f87432a35fa79402e303
useregistrado <oscar.vargas07@uptc.edu.co>
2021-05-08 23:06:56 -0500

    se renombraron los archivos y se eliminaron los textview

--- app/src/main/java/com/example/taskapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/taskapp/ui/home/HomeFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.taskapp.R
 
@@ -21,11 +19,8 @@ class HomeFragment : Fragment() {
     ): View? {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = "Area para mostrar los cursos"
-        })
+        val root = inflater.inflate(R.layout.cursos, container, false)
+
         return root
     }
 }
--- app/src/main/java/com/example/taskapp/ui/slideshow/SlideshowFragment.kt
+++ app/src/main/java/com/example/taskapp/ui/slideshow/SlideshowFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.taskapp.R
 
@@ -21,11 +19,7 @@ class SlideshowFragment : Fragment() {
     ): View? {
         slideshowViewModel =
                 ViewModelProvider(this).get(SlideshowViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
-        val textView: TextView = root.findViewById(R.id.text_slideshow)
-        slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = "Area para modificar las notificaciones"
-        })
+        val root = inflater.inflate(R.layout.notificaciones, container, false)
         return root
     }
 }
